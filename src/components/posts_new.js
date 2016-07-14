2024abf69375cf22f7abf40581e9c343f8c85d6b
Fareez Ahmed <fareez.ahmed@ge.com>
2016-07-14 14:05:12 -0400

    add attributes for form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,22 +2,32 @@ import React, {Component} from 'react';
 import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  //redux form adds to this.props
   render () {
+    // fields object, equivalent to: const title = this.props.fields.title
+    const {fields: {title, categories, content}, handleSubmit} = this.props;
+    // console.log(title);
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
+    
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control"/>
+          // with {...title}
+          // everything will show up like this: onChange={title.onChange}
+          <input type="text" className="form-control" {...title}/>
         </div>
+
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control" {...categories}/>
         </div>
+        
         <div className="form-group">
           <label>Content</label>
-          <textarea type="text" className="form-control"/>
+          <textarea type="text" className="form-control" {...content}/>
         </div>
+        
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
