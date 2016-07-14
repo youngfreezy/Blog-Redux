fbaa7098631cc36b7326953286310a591bfebf03
Fareez Ahmed <fareez.ahmed@ge.com>
2016-07-14 12:55:41 -0400

    new post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
-export default () => {
-  return <div>List of blog posts</div>
+class PostsIndex extends Component {
+  render () {
+    return(
+      <div>
+        <div className = "text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+      List of blog posts
+      </div>
+    )
+  }
+  componentWillMount(){
+    this.props.fetchPosts();
+  }
 }
+
+export default connect(null, {fetchPosts})(PostsIndex);
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
+import PostsNew from './components/posts_new';
 
 import App from './components/app';
 import PostsIndex from './components/posts_index';
@@ -8,6 +9,7 @@ export default (
   
   <Route path="/" component={App}>
     <IndexRoute component={PostsIndex}/>
+    <Route path="/posts/new" component={PostsNew}/>
   </Route>
 
   )
